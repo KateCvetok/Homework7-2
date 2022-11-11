6787b43b54b91073b8e8783ebd8eb207e8f70498
Kate <cvetok_spb@bk.ru>
2022-11-11 11:47:39 +0300

    Task5

---
+++ 5/Program.cs
@@ -0,0 +1,50 @@
+﻿// В матрице чисел найти сумму элементов главной диагонали.
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+int Sum(int[,] matr)
+{
+    int sum = 0;
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+       sum += matr[i, i];
+        
+    }
+    return sum;
+}
+
+Console.WriteLine("Введите количество строк: ");
+int m = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите количество столбцов: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+
+
+int[,] matrix = new int[m, n];
+Console.WriteLine();
+FillArray(matrix);
+PrintArray(matrix);
+Console.WriteLine();
+
+int sum = Sum(matrix);
+Console.WriteLine($"Сумма элементов главной диагонали {sum}");
+
+
