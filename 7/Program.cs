82350d47f58efd054568cdef8d6cdb3ef8b10b87
Kate <cvetok_spb@bk.ru>
2022-11-11 11:52:38 +0300

    Task7

---
+++ 7/Program.cs
@@ -0,0 +1,44 @@
+﻿//Написать программу, которая обменивает элементы первой строки и последней строки.
+
+void FillArray(int[,] matr) 
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+void PrintArray(int[,] matr) 
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+void Change(int[,] matr)
+{ 
+    int temp = 0;
+    for (int i = 0; i < matr.GetLength(1); i++)
+    {
+        temp = matr[0, i];
+        matr[0, i] = matr[matr.GetLength(0) - 1, i];
+        matr[matr.GetLength(0) - 1, i] = temp;
+    }
+}
+Console.WriteLine("Введите количество строк: ");
+int m = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите количество столбцов: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+int[,] matrix = new int[m, n];
+Console.WriteLine("Изначальный массив: ");
+FillArray(matrix);
+PrintArray(matrix);
+Console.WriteLine("Массив в котором поменяли элементы первой и последней строки: ");
+Change(matrix);
+PrintArray(matrix);
